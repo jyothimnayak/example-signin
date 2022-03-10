b5318b28cfb4bcfdbae5836c7de0e2dad5f6c3a5
Jyothi M <jyothi.m@in.abb.com>
2022-03-10 16:36:04 +0530

    signin formgroup addition

--- app/src/app/signin/signin.component.ts
+++ app/src/app/signin/signin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 
 @Component({
@@ -7,8 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signin.component.scss']
 })
 export class SigninComponent implements OnInit {
+  signinForm!: FormGroup;
+  constructor(private fb:FormBuilder) { 
+        this.signinForm=this.fb.group({
+          email:['',Validators.required],
+          password:['',Validators.required],
+        })
+      }
 
-  constructor() { }
+      public addItem():void{
+        console.log(this.signinForm.value)
+       this.reset();  
+      }
+      reset(){
+        this.signinForm.reset();
+      }
+  
 
   ngOnInit(): void {
   }
